49e32b29922175f6a3d8e0c3102986a75aacf5a5
project_demo <nilanjanm@testvagrant.com>
2023-09-19 12:45:57 +0530

    demo app

---
+++ README.md
@@ -0,0 +1,15 @@
+# my_flutter_proj_demo
+
+NOTE: Connect an android emulator and perform any debugs
+
+# x-x-x-x-x-x-x-x-x-x-x-x-x--x-x
+
+To run the app on a device use below command:
+"flutter run lib/main.dart --dart-define-from-file=.secrets/uat.json"
+
+# x-x-x-x-x-x-x-x-x-x-x-x-x--x-x
+
+To run the integration test use below command:
+"flutter test integration_test/app_test.dart --dart-define-from-file=.secrets/uat.json"
+
+# x-x-x-x-x-x-x-x-x-x-x-x-x--x-x
---
+++ integration_test/app_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:my_flutter_proj_demo/main.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('tap on the floating action button, verify counter',
+        (tester) async {
+      // Load app widget.
+      await tester.pumpWidget(const MyApp());
+
+      // Verify the counter starts at 0.
+      expect(find.text('0'), findsOneWidget);
+
+      // Finds the floating action button to tap on.
+      final Finder fab = find.byTooltip('Increment');
+
+      // Emulate a tap on the floating action button.
+      await tester.tap(fab);
+
+      // Trigger a frame.
+      await tester.pumpAndSettle();
+
+      // Verify the counter increments by 1.
+      expect(find.text('1'), findsOneWidget);
+
+      expect(find.text('Flutter Demo #envVar1 Home Page'), findsOneWidget);
+    });
+  });
+}
